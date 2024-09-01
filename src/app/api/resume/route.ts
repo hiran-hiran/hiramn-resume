e981ce1f7c58c97589c25d0924111496ed5a0419
hiran-hiran <nozomu.hirata@timee.co.jp>
2024-09-01 16:31:43 +0900

    fix: build error

--- src/app/api/resume/route.ts
+++
@@ -1,9 +0,0 @@
-import { newtClient } from "@/lib/newt";
-
-export async function GET() {
-  return await newtClient.getContent({
-    appUid: "resume-site",
-    modelUid: "resume",
-    contentId: "6425acf886e7de37b829ac7a",
-  });
-}
