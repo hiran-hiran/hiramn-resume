38da3e05f93841bc9b6646cf9c282a379f11531f
hiran-hiran <nozomu.hirata@timee.co.jp>
2025-02-07 22:49:54 +0900

    fix: cache trouble

--- src/shared/lib/hono/index.ts
+++ src/shared/lib/hono/index.ts
@@ -1,4 +1,11 @@
 import type { AppType } from "@/app/api/[[...route]]/route";
 import { hc } from "hono/client";
 
-export const client = hc<AppType>(process.env.NEXT_PUBLIC_API_ROUTE as string);
+export const client = hc<AppType>(process.env.NEXT_PUBLIC_API_ROUTE as string, {
+  // https://zenn.dev/yoshikouki/articles/4e351b29a3cc56
+  fetch: (input: RequestInfo | URL, requestInit?: RequestInit) =>
+    fetch(input, {
+      cache: "no-cache",
+      ...requestInit,
+    }),
+});
